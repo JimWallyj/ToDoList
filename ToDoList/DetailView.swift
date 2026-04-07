a319ef286e4bf3068400759f42614f6cb4329576
Jim Walejko <jwalejko@me.com>
2026-04-06 22:27:54 -0700

    Prof. John Gallaugher SwiftUI Programming course - Chapter 5.3 = Build the ToDo list user interface with Toolbars, Sheets FullScreenCover; toggle interface elements to disable and enabled.

--- ToDoList/DetailView.swift
+++ ToDoList/DetailView.swift
@@ -8,30 +8,61 @@
 import SwiftUI
 
 struct DetailView: View {
-    var passedValue: String  //  Don't initialize - it will be passed from the parent view
-    @Environment(\.dismiss) private var dismiss
+    @State var toDo: String
+    @State private var reminderIsOn = false
+//    @State private var dueDate = Date.now + 60*60*24
+    @State private var dueDate = Calendar.current.date(byAdding: .day, value: 1, to: Date.now)!
+    @State private var notes = ""
+    @State private var isCompleted = false
     
+    @Environment(\.dismiss) private var dismiss
     var body: some View {
-        VStack{
-            Image(systemName: "swift")
-                .resizable()
-                .scaledToFit()
-                .foregroundStyle(.orange)
-            Text("You are a Swifty Legend! \nAnd you passed over the value \(passedValue)")
-                .font(.largeTitle)
-                .multilineTextAlignment(.center)
-            
-            Spacer()
-            
-            Button("Get Back!"){
-                dismiss()
+            List{
+                TextField("Enter to do here", text: $toDo)
+                    .font(.title)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(.vertical)
+                    .listRowSeparator(.hidden)
+                
+                Toggle("Set Reminder:", isOn: $reminderIsOn)
+                    .padding(.top)
+                    .listRowSeparator(.hidden)
+                
+                DatePicker("Date:", selection: $dueDate)
+                    .listRowSeparator(.hidden)
+                    .padding(.bottom)
+                    .disabled(!reminderIsOn)
+                
+                Text("Notes:")
+                    .padding(.top)
+                
+                TextField("Notes", text: $notes, axis: .vertical)
+                    .textFieldStyle(.roundedBorder)
+                    .listRowSeparator(.hidden)
+                
+                Toggle("Completed:", isOn: $isCompleted)
+                    .padding(.top)
+                    .listRowSeparator(.hidden)
             }
-            .buttonStyle(.borderedProminent)
-        }
-        .padding()
+            .listStyle(.plain)
+            .navigationBarBackButtonHidden()
+            .toolbar{
+                ToolbarItem(placement: .topBarLeading){
+                    Button("Cancel"){
+                        dismiss()
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing){
+                    Button("Save"){
+                        //TODO: Add Save Code Here
+                    }
+                }
+            }
     }
 }
 
 #Preview {
-    DetailView(passedValue: "Item 1")
+    NavigationStack{
+        DetailView(toDo: "")
+    }
 }
--- ToDoList/ToDoListView.swift
+++ ToDoList/ToDoListView.swift
@@ -13,20 +13,38 @@ struct ToDoListView: View {
                  "Change the World",
                  "Bring the Awesome",
                  "Take a Vacation"]
+    @State private var sheetIsPresented = false
     
     var body: some View {
         NavigationStack {
             List{
                 ForEach(toDos, id: \.self){ toDo in
                     NavigationLink{
-                        DetailView(passedValue: toDo)
+                        DetailView(toDo: toDo)
                     }label: {
                             Text(toDo)
                         }
+                    .font(.title2)
                     }
                 }
             .navigationTitle("To Do List:")
+            .navigationBarTitleDisplayMode(/*@START_MENU_TOKEN@*/.automatic/*@END_MENU_TOKEN@*/)
             .listStyle(.plain)
+            //  fullScreenCover(isPresented: $sheetIsPresented){
+            .sheet(isPresented: $sheetIsPresented){
+                NavigationStack {
+                    DetailView(toDo: "")
+                }
+            }
+            .toolbar{
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button{
+                        sheetIsPresented.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
         }
     }
 }
